557caa6d2d5628f3ec6780ba322343dfdc952933
MarcoFarinetti <farinetti94@gmail.com>
2021-06-27 18:28:43 +0200

    sample main

--- src/main/java/marco/farinetti/taskmanager/Application.java
+++ src/main/java/marco/farinetti/taskmanager/Application.java
@@ -1,5 +1,6 @@
 package marco.farinetti.taskmanager;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,12 +10,13 @@ import marco.farinetti.taskmanager.impl.TaskManagerImpl;
 import marco.farinetti.taskmanager.impl.TaskManagerPriorityImpl;
 import marco.farinetti.taskmanager.model.Priority;
 import marco.farinetti.taskmanager.model.SortingParam;
+import marco.farinetti.taskmanager.model.Task;
 import marco.farinetti.taskmanager.spi.TaskManager;
 
 public class Application {
 
   public static void main(String[] args) {
-//    ExecutorService threadpool = Executors.newCachedThreadPool();
+    ExecutorService threadpool = Executors.newCachedThreadPool();
 
     TaskManager tm = new TaskManagerImpl();
     TaskManager tmFifo = new TaskManagerFIFOImpl();
@@ -24,28 +26,42 @@ public class Application {
     tm.add(Priority.MEDIUM);
     tm.add(Priority.LOW);
 
-
-    tmFifo.add(Priority.HIGH);
-    tmFifo.add(Priority.MEDIUM);
+    tmFifo.add(Priority.LOW);
+    tmFifo.add(Priority.LOW);
     tmFifo.add(Priority.LOW);
 
     tmPrio.add(Priority.HIGH);
     tmPrio.add(Priority.LOW);
     tmPrio.add(Priority.MEDIUM);
-//    Future<?> submit1 = threadpool.submit(() -> tm.add(Priority.HIGH));
-//    Future<?> submit2 = threadpool.submit(() -> tm.add(Priority.HIGH));
 
+    Callable<Task> taskCallable1 = () -> tm.add(Priority.HIGH);
+    Future<?> submit1 = threadpool.submit(taskCallable1);
+    Future<?> submit2 = threadpool.submit(taskCallable1);
+    Callable<Task> taskCallable2 = () -> tmFifo.add(Priority.HIGH);
+    Future<?> submit3 = threadpool.submit(taskCallable2);
+    Future<?> submit4 = threadpool.submit(taskCallable2);
+    Callable<Task> taskCallable3 = () -> tmPrio.add(Priority.HIGH);
+    Future<?> submit5 = threadpool.submit(taskCallable3);
+    Future<?> submit6 = threadpool.submit(taskCallable3);
+
+    try {
+      submit1.get();
+      submit2.get();
+      submit3.get();
+      submit4.get();
+      submit5.get();
+      submit6.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
+
+    System.out.print("\nTM Default: ");
     tm.list(SortingParam.CREATION);
+    System.out.print("\nTM FIFO: ");
     tmFifo.list(SortingParam.CREATION);
+    System.out.print("\nTM Prio: ");
     tmPrio.list(SortingParam.CREATION);
 
-//    try {
-//      submit1.get();
-//      submit2.get();
-//    } catch (InterruptedException | ExecutionException e) {
-//      e.printStackTrace();
-//    }
-//    tm.list(SortingParam.CREATION);
-//    threadpool.shutdown();
+    threadpool.shutdown();
   }
 }
